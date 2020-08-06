eb258a7c7578a3e9687fb5277efbb24ca53af00f
require64bits <tankoaraujo@gmail.com>
2020-08-06 18:54:14 -0300

    Transição para o selenium usando o firefox

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 
-service = webdriver.edge.service.Service("C:\\Users\\tanko\\Downloads\\edgedriver_win64\\msedgedriver")
-browser = webdriver.Edge(service=service)
+browser = webdriver.Firefox(executable_path="/home/avatar/Projects/webscrapingPython/geckodriver")
 
 Lane = {}
 browser.get("https://br.op.gg/champion/statistics")
 content = browser.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div[6]/div/div[2]/div/div[1]/div/table/tbody[1]/tr[1]/td[5]")
 print(content.text)
-
