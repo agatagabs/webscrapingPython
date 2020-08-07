9d5f7dfd3560f42f543fe845dd461632cbc519fa
require64bits <tankoaraujo@gmail.com>
2020-08-07 12:09:13 -0300

    Quase completo, precisa dar uma refinada :p

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ from selenium import webdriver
 
 browser = webdriver.Firefox(executable_path="/home/avatar/Projects/webscrapingPython/geckodriver")
 
-Lane = {}
+Lane = {"top": [], "jungle": [], "middle": [], "bottom": [], "support": []}
+
 browser.get("https://br.op.gg/champion/statistics")
-content = browser.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div[6]/div/div[2]/div/div[1]/div/table/tbody[1]/tr[1]/td[5]")
-print(content.text)
+name = browser.find_elements_by_css_selector("td:nth-child(4)")
+winRate = browser.find_elements_by_css_selector("td:nth-child(5)")
+
+while len(name) != 51:
+    name.pop()
+
+while len(winRate) != 51:
+    winRate.pop()
+
+for x, y in zip(name, winRate):
+    for z in x.text.split("\n", 1)[1].split(", "):
+        Lane[z.lower()].append([x.text.split("\n")[0], y.text])
+
+print(Lane)
+
+browser.close()
