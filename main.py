6437b592e18747945ff1c1a3413050995844c6df
require64bits <tankoaraujo@gmail.com>
2020-08-07 13:13:51 -0300

    Quase lá!

--- main.py
+++ main.py
@@ -2,22 +2,35 @@ from selenium import webdriver
 
 browser = webdriver.Firefox(executable_path="/home/avatar/Projects/webscrapingPython/geckodriver")
 
-Lane = {"top": [], "jungle": [], "middle": [], "bottom": [], "support": []}
+Lane = {"Top": [], "Jungle": [], "Middle": [], "Bottom": [], "Support": []}
 
 browser.get("https://br.op.gg/champion/statistics")
-name = browser.find_elements_by_css_selector("td:nth-child(4)")
-winRate = browser.find_elements_by_css_selector("td:nth-child(5)")
+element = browser.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div[6]/div/div[1]/ul/li[2]/a")
+element.click()
+name = browser.find_elements_by_css_selector("td:nth-child(3)")
+winRate = browser.find_elements_by_css_selector("td:nth-child(4)")
 
-while len(name) != 51:
+print(len(name))
+
+print(name[0].text)
+print(winRate[0].text)
+
+
+while len(name) != 149:
     name.pop()
 
-while len(winRate) != 51:
+while len(winRate) != 149:
     winRate.pop()
+
+print(name[0].text)
+print(winRate[0].text)
 
 for x, y in zip(name, winRate):
     for z in x.text.split("\n", 1)[1].split(", "):
-        Lane[z.lower()].append([x.text.split("\n")[0], y.text])
+        Lane[z].append([x.text.split("\n")[0], y.text])
 
-print(Lane)
+for x in Lane:
+    print(x)
+    print(Lane[x])
 
 browser.close()
